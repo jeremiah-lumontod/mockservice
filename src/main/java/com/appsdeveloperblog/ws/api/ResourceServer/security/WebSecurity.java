e8235380389ead71bd800993319304dbe4e1d317
Jerry Lumontod <>
2024-02-13 11:51:04 +0800

    init files

---
+++ src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/security/WebSecurity.java
@@ -0,0 +1,71 @@
+package com.appsdeveloperblog.ws.api.ResourceServer.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+
+@EnableGlobalMethodSecurity(securedEnabled=true, prePostEnabled=true)
+@EnableWebSecurity
+public class WebSecurity {
+	
+	@Bean
+	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
+		JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+
+		//http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+		http
+		.authorizeHttpRequests((authz) -> authz.requestMatchers(HttpMethod.GET, "/users/status/check")
+				// .hasAuthority("SCOPE_profile")
+				.hasRole("developer")
+				//.hasAnyAuthority("ROLE_developer")
+				.anyRequest().authenticated())
+				.oauth2ResourceServer(
+						oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)));
+
+		return http.build();
+
+
+
+/*		http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
+				.authorizeHttpRequests((authz) -> authz.requestMatchers(HttpMethod.GET, "/users/status/check")
+						// .hasAuthority("SCOPE_profile")
+						.hasRole("developer")
+						// .hasAnyAuthority("ROLE_developer")
+						// .hasAnyRole("devleoper","user")
+						.anyRequest().authenticated())
+				.oauth2ResourceServer(
+						oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)));
+
+		return http.build();*/
+
+	}
+
+
+/*	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET","POST"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfiguration);
+		return source;
+	}*/
+
+}
